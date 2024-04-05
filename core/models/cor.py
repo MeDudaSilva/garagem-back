114d130d2d2b9f2d81211f13b31043ddba0c37a1
MeDudaSilva <mariaeduarda.11.me4@gmail.com>
2024-04-05 15:40:34 -0300

    mudando o plural da model cor

--- core/models/cor.py
+++ core/models/cor.py
@@ -4,4 +4,6 @@ class Cor(models.Model):
     nome = models.CharField(max_length=100)
     def __str__(self):
         return f"{self.nome} {self.id}"
-    
+    class Meta:
+        verbose_name = "Cor"
+        verbose_name_plural = "Cores"
